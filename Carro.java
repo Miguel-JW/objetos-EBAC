5c2fc3bef89c9784890e7f2b2339c2ba2d5f48a5
Miguel-JW <conteudojurassicojw@gmail.com>
2026-05-22 10:30:50 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    String nome;
+    String marca;
+    int ano;
+    double preco;
+
+    public Carro(String nome, String marca, int ano, double preco) {
+        this.nome = nome;
+        this.marca = marca;
+        this.ano = ano;
+        this.preco = preco;
+    }
+}
